3c22a71e24dc1ee6770837d63b6d5e7ad39d0f4a
James Sumners <jsumners@users.noreply.github.com>
2012-05-23 13:23:24 -0400

    Added an about box and a LICENSE file.

---
+++ WinLN/AboutForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace com.jrfom.winln {
+  public partial class AboutForm : Form {
+    public AboutForm() {
+      InitializeComponent();
+
+      versionNumber.Text = Application.ProductVersion;
+    }
+
+    private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+      ProcessStartInfo startInfo = new ProcessStartInfo(url.Text);
+      Process.Start(startInfo);
+    }
+  }
+}
